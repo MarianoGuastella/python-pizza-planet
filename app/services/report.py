7b421d808d381a0aa35ad6b62c71d80dbad2b8bf
MarianoGuastella <marianoguastella@hotmail.com>
2024-08-18 15:09:32 -0300

    feat: ui updated

--- app/services/report.py
+++ app/services/report.py
@@ -11,11 +11,11 @@ def get_report():
     most_requested_ingredient = report_controller.generate_report()
 
     report_controller.set_strategy(MonthWithMostRevenueStrategy())
-    month_with_more_revenue = report_controller.generate_report()
+    month_with_most_revenue = report_controller.generate_report()
 
     report_controller.set_strategy(TopCustomersStrategy())
     top_customers = report_controller.generate_report()
 
     return jsonify(most_requested_ingredient=most_requested_ingredient, 
-                   month_with_more_revenue=month_with_more_revenue, 
+                   month_with_most_revenue=month_with_most_revenue, 
                    top_customers=top_customers)
